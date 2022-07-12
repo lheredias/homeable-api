5182eb79be839c8a34c50fbf8bed07303710e730
lheredias <herediasotelo@gmail.com>
2022-07-11 22:16:26 -0500

    signup and login working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_003354) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_031449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,7 +92,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_003354) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["token"], name: "index_users_on_token", unique: true
+    t.datetime "token_created_at"
+    t.index ["token", "token_created_at"], name: "index_users_on_token_and_token_created_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
