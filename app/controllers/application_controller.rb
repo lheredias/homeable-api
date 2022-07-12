def77cae3439c33d6855396fbd241ee271dbc538
lheredias <herediasotelo@gmail.com>
2022-07-11 21:47:58 -0500

    add signup route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,26 @@
 class ApplicationController < ActionController::API
-  # This is neened because ActionControlle::API doesn't include the module by default
-  # This module is required for using authenticate_with_http_token method
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  before_action :authorize
+  before_action :require_login!
 
-def current_user
-  @current_user ||= authenticate_token
-end
-
-def authorize
-  authenticate_token || respond_unauthorized('Access denied')
-end
-
-private
-
-def respond_unauthorized(message)
-  error = { unauthorized: message }
-  render json: error, status: :unauthorized
-end
-
-def authenticate_token
-  authenticate_with_http_token do |token, _options|
-    User.find_by(token: token)
+  def require_login!
+    authenticate_token || render_unauthorized('Access denied')
   end
-end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  private
+
+  def render_unauthorized(message)
+    errors = { errors: message }
+    render json: errors, status: :unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      User.where(['token = ? and token_created_at >= ?', token, 1.hour.ago]).first
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  post    '/login'    => 'sessions#create'
+  delete  '/logout'   => 'sessions#destroy'
+  post    '/signup'   => 'users#create'
+
+  resource :profile, except: %i[index create], controller: :users
 end
