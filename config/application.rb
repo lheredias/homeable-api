fbc91e9b50aef11440947501da07188dba4ebadd
lheredias <herediasotelo@gmail.com>
2022-07-12 13:05:48 -0500

    tryout04

--- config/application.rb
+++ config/application.rb
@@ -35,12 +35,12 @@ module HomeableApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins ''
-        resource '', :headers => :any, :methods => [:get, :post, :options]
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, ,:delete, :options]
       end
     end
+
   end
 end
