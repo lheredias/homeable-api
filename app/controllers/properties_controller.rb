b19fd881060afecb1cd1f33ab34efdb3d6d8c463
lheredias <herediasotelo@gmail.com>
2022-07-13 02:35:25 -0500

    property queries filter to module

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,48 +1,11 @@
 class PropertiesController < ApplicationController
   skip_before_action :require_login!, only: %i[index show]
   # GET /properties
+  include PropertiesHelper
   def index
     properties = Property.all
-    if params[:address]
-      properties = properties.where("address ILIKE ?", "%"+ params[:address] +"%")
-    end
-
-    if params[:operation]
-      properties = properties.where(operation:params[:operation].downcase())
-    end
-
-    if params[:property_type]
-      properties = properties.where(property_type: params[:property_type])
-      # Property.where(property_type: ["apartment","house"])
-    end
-
-    if params[:pets]
-      properties = properties.where(pets: params[:pets])
-    end
-
-    if params[:min_price]
-      properties = properties.where("price >= ?", params[:min_price])
-    end
-
-    if params[:max_price]
-      properties = properties.where("price <= ?", params[:max_price])
-    end
-
-    if params[:min_area]
-      properties = properties.where("area >= ?", params[:min_area])
-    end
-
-    if params[:max_area]
-      properties = properties.where("area <= ?", params[:max_area])
-    end
-
-    if params[:bedrooms]
-      properties = properties.where("bedrooms >= ?", params[:bedrooms])
-    end
-
-    if params[:bathrooms]
-      properties = properties.where("bedrooms >= ?", params[:bathrooms])
-    end
+    
+    properties = query_filter(properties)
 
     render json: properties
   end
---
+++ app/helpers/properties_helper.rb
@@ -0,0 +1,46 @@
+module PropertiesHelper
+  def query_filter(properties)
+    if params[:address]
+      properties = properties.where("address ILIKE ?", "%"+ params[:address] +"%")
+    end
+
+    if params[:operation]
+      properties = properties.where(operation:params[:operation].downcase())
+    end
+
+    if params[:property_type]
+      properties = properties.where(property_type: params[:property_type])
+      # Property.where(property_type: ["apartment","house"])
+    end
+
+    if params[:pets]
+      properties = properties.where(pets: params[:pets])
+    end
+
+    if params[:min_price]
+      properties = properties.where("price >= ?", params[:min_price])
+    end
+
+    if params[:max_price]
+      properties = properties.where("price <= ?", params[:max_price])
+    end
+
+    if params[:min_area]
+      properties = properties.where("area >= ?", params[:min_area])
+    end
+
+    if params[:max_area]
+      properties = properties.where("area <= ?", params[:max_area])
+    end
+
+    if params[:bedrooms]
+      properties = properties.where("bedrooms >= ?", params[:bedrooms])
+    end
+
+    if params[:bathrooms]
+      properties = properties.where("bedrooms >= ?", params[:bathrooms])
+    end
+
+    properties
+  end
+end
